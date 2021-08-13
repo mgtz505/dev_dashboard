fcadef19ffb68139281fbc933ae069538be28668
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-13 16:00:53 -0400

    Data Map was successful

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import axios from "axios"
-
-//Global Variables
-const KEY = process.env.REACT_APP_API_KEY
-const URL = `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${KEY}`
-
-
-const getNews = () => {
-   
-     axios.get(URL).then((response) => {
-         console.log(response)
-     })
-     .catch(console.error)
-}
-getNews()
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const News = () => {
-    return (
-        <div>
-            <h2>See Today's Top News from the NYT</h2>
-        </div>
-    );
+  //Global Variables
+  const KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${KEY}`;
+
+  //State
+  const [stories, setStories] = useState([]);
+
+  const getNews = () => {
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response.data.results);
+        setStories(response.data.results);
+      })
+      .catch(console.error);
+  };
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <div>
+      <h2>See Today's Top News from the NYT</h2>
+
+      {stories.map((story, index) => {
+        return (
+          <div key={index} className="story-container">
+            <h3 className="story-title">{story.title}</h3>
+            <h4 className="story-abstract">{story.abstract}</h4>
+            <img alt="storyimage" src={story.multimedia[0].url}/>
+            <a target="_blank" href={story.short_url}>View on NYT</a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default News;
