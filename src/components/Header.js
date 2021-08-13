8f6509654cf5a575d56cd2b19716af1473d487c3
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-13 14:59:04 -0400

    Set up first pass at file structure and also installed styled components dependency

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to Dev Dashboard</h1>
+        </div>
+    );
+};
+
+export default Header;
