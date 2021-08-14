684c511c83b9c652094934cb526de7d1f67cac93
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-14 11:31:42 -0400

    Starting construction of pomodoro timer

--- src/components/HQ.js
+++ src/components/HQ.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import News from "./News";
+import Pomodoro from "./Pomodoro";
 
 const HQ = () => {
     return (
-        <div>
+        <div className="widgit-grid">
             <h2>What are we going to build Today?</h2>
-
+            <Pomodoro />
             <News />
         </div>
     );
---
+++ src/components/Pomodoro.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import "../styles/pomodoro.css";
+
+const Pomodoro = () => {
+
+
+
+    return (
+        <div className="pomodoro-widgit">
+            <h2>Pomodoro Timer</h2>
+        </div>
+    );
+};
+
+export default Pomodoro
