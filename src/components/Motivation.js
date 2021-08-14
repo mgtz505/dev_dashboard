beae03cfba44306c73b1097daf1dbeccf07b14f5
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-14 16:05:42 -0400

    Working on being able to call both APIs in Motivation.js

--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -4,30 +4,43 @@ import axios from "axios";
 
 const Motivation = () => {
     const [photo, setPhoto] = useState();
+    const [quote, setQuote] = useState([]);
 
-const URL = "https://picsum.photos/300/200.jpg";
+const URL_PHOTO = "https://picsum.photos/300/200.jpg";
+const URL_QUOTE = "https://quote-garden.herokuapp.com/api/v3/quotes";
 
 const handleRequest = useCallback(() => {
     setPhoto();
     getPhoto();
-},[photo])
+    // setQuote();
+    getQuote();
+},[photo, quote])
 
 const getPhoto = () => {
-    axios.get(URL)
+    axios.get(URL_PHOTO)
     .then((response) => {
         console.log(response.config.url);
-        setPhoto(response.config.url);
-        
+        setPhoto(response.config.url); 
+    })
+}
+const getQuote = () => {
+    axios.get(URL_QUOTE)
+    .then((response) => {
+        let text = (response.data.data[0].quoteText);
+        let author = (response.data.data[0].quoteAuthor);
+        setQuote([text,author])
     })
 }
 
+console.log(quote)
 
-console.log(photo)
     return (
         <div className="widgit">
             <h2>Get Motivated</h2>
             <button onClick={() => handleRequest()}>X</button> 
             <img className="motivation-image" alt="motivationalPhoto" src={photo}/>
+            <h3>{quote[0]}</h3>
+            <h4>-{quote[1]}</h4>
         </div>
     );
 };
