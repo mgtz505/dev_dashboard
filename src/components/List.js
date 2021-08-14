a61a89eeac454587779bfc9b417eaba88376e28e
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-14 14:31:47 -0400

    Reorganized todo component to better display props

--- src/components/List.js
+++ src/components/List.js
@@ -1,56 +1,26 @@
 import React, { useState } from 'react';
 
-const List = () => {
-const [todos, setTodos] = useState([
-    {text: "EXAMPLE TASK A", isComplete: false},
-    {text: "EXAMPLE TASK B", isComplete: false},
-    {text: "EXAMPLE TASK C", isComplete: false},
-    {text: "EXAMPLE TASK D", isComplete: false},
-]);
-
-const Todo = () => {
-
-
-    const handleComplete = (index) => {
-        const newTodos = [...todos];
-        let target = newTodos[index];
-        if (target.isComplete === false) {
-            target.isComplete = true
-        } else {
-            if (target.isComplete === true) {
-                target.isComplete = false
-            }
-        }
-        setTodos(newTodos);
-    }
+const Todo = ({ todo, index, handleComplete, handleDelete }) => {
 
     return (
         <div className="todo-container">
-            {todos.map((todo, index) => {
-
-                return (  
+    
                 <div 
                 className="todo-entry"
                 style={{textDecoration: todo.isComplete ? "line-through" : ""}}>
                     <h2 key={index}>{todo.text}</h2>
                     <div className="control-panel">
-                        <button onClick={() => handleComplete(index)}>✔️ </button>
+                        <button onClick={() => handleComplete(index)}>✔️</button>
+                        <button onClick={() => handleDelete(index)}>🗑</button>
                     </div>
                 </div>
-                )
-              
-            })}
+           
         </div>
     )
 }
-
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
     const [value, setValue] = useState("");
 
-const addTodo = (text) => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-}
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -74,11 +44,52 @@ const handleSubmit = (e) => {
         </div>
     )
 }
+const List = () => {
+const [todos, setTodos] = useState([
+    {text: "EXAMPLE TASK A", isComplete: false},
+    {text: "EXAMPLE TASK B", isComplete: false},
+    {text: "EXAMPLE TASK C", isComplete: false},
+    {text: "EXAMPLE TASK D", isComplete: false},
+]);
 
+const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+}
+
+const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+}
+
+const handleComplete = (index) => {
+    console.log(index)
+    const newTodos = [...todos];
+    let target = newTodos[index];
+    console.log(target)
+    if (target.isComplete === false) {
+        target.isComplete = true
+    } else {
+        if (target.isComplete === true) {
+            target.isComplete = false
+        }
+    }
+    setTodos(newTodos);
+}
     return (
         <div>
-            <Todo />
-            <TodoForm />
+            {todos.map((todo, index) => {
+                return (
+                    <Todo
+                    index={index}
+                    todo={todo}
+                    handleComplete={handleComplete}
+                    handleDelete={handleDelete} />
+                )
+            })}
+            <TodoForm
+            addTodo={addTodo} />
         </div>
     );
 };
