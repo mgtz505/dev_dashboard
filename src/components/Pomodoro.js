e0b69513c409db46c6cadabbad8bfec04585ec76
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-14 11:43:27 -0400

    Completed first pass at pomodoro widgit. Going to add buttons and  other functionality now.

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -2,12 +2,46 @@ import React, { useState, useEffect } from 'react';
 import "../styles/pomodoro.css";
 
 const Pomodoro = () => {
+const [minutes, setMinutes] = useState(25);
+const [seconds, setSeconds] = useState(0);
+const [message, setMessage] = useState(false);
 
+const timerMinutes = minutes < 10 ? `0:${minutes}` : minutes;
+const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
+useEffect(() => {
+    let interval = setInterval(() => {
+        clearInterval(interval)
+    
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59);
+            setMinutes(minutes -1);
+          } else {
+            let minutes = message ? 24 : 4
+            let seconds = 59
+
+            setSeconds(seconds);
+            setMinutes(minutes);
+            setMessage(!message);
+          }
+        } else {
+         setSeconds(seconds - 1);
+        }
+      }, 1000)
+    }, [seconds]);
 
     return (
         <div className="pomodoro-widgit">
+            <div className="timer-body">
             <h2>Pomodoro Timer</h2>
+                <div className="message-body">
+                    {message && <div>Break! Resume in:</div>}
+                </div>
+                <div className="timer-body">
+                {timerMinutes}:{timerSeconds}
+                </div>
+            </div>
         </div>
     );
 };
