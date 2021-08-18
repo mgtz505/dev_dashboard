2ea97d1d6bfcd145c3cafd7bd6b7be11fd12381d
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-18 16:35:19 -0400

    About to install Octokit dependency. API key safe in .env

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 
+const URL = "https://api.github.com/users/mgtz505";
+
 const GitHub = () => {
+    const[ghData, setGhData] = useState()
+
+    const getGitHubData = () => {
+        axios.get(URL)
+        .then((response => {
+            console.log(response);
+            setGhData(response.data)
+        }))
+    }
+
+console.log(process.env.REACT_APP_GH_KEY)
+
     return (
         <div className="widgit">
             <h2>GitHub</h2>
+            <button onClick={() => getGitHubData()}>Get GH Data</button>
         </div>
     );
 };
