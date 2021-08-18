db14b1c39e4918e597ee2ef7bb45588a6ddb1014
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-18 14:30:01 -0400

    Made requisite imports from code-mirror. BStarting buildout of editor component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/duotone-light.css";
+import "codemirror/mode/javascript/javascript"
 
 const Editor = () => {
     return (
         <div className="widgit">
             <h2>Editor</h2>
+            <div></div>
+
+            <div>
+                <iframe 
+                title="output"
+                sandbox="allow-scripts"
+                frameBorder="0"
+                width="100%"
+                height="100%"/>
+                
+            </div>
+
         </div>
     );
 };
