ed457ec2d0ccc336570a61058880b4f6194280da
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-18 17:07:44 -0400

    GitHub API working via octokit. Calls to API are successful. Exploring Promise Data to see what I'd like displayed

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect } from 'react';
 import axios from "axios";
+import { Octokit } from "@octokit/core"
 
 const URL = "https://api.github.com/users/mgtz505";
+const KEY = process.env.REACT_APP_GH_KEY;
 
 const GitHub = () => {
     const[ghData, setGhData] = useState()
 
+    const [commits, setCommits] = useState([]);
+    const octokit = new Octokit({auth: KEY })
+
+    useEffect(() => {
+        const owner = "mgtz505"
+        const repo = "project_euler"
+        const perPage = 5;
+
+        const fiveMostRecentCommits = octokit.request(
+            `GET /repos/${owner}/${repo}/commits`, { owner, repo, per_page: perPage }
+            ).then((response) => {
+                console.log(response.data)
+                setCommits(response.data);
+            });
+
+    },[])
+
+    console.log(commits)
     const getGitHubData = () => {
         axios.get(URL)
         .then((response => {
@@ -14,12 +34,20 @@ const GitHub = () => {
         }))
     }
 
-console.log(process.env.REACT_APP_GH_KEY)
-
     return (
         <div className="widgit">
             <h2>GitHub</h2>
             <button onClick={() => getGitHubData()}>Get GH Data</button>
+            <ul>
+               {commits.map(commit => (
+                   <>
+                   <li key={commit.id}>
+                       {commit.author.login}: {commit.commit.message}
+                    </li>
+                       <h5>{commit.commit.url}</h5>
+                </>
+               ))}
+            </ul>
         </div>
     );
 };
