caccaa43733b48742601010f8d5c4f56402eae2f
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-19 13:59:41 -0400

    Testing refresh button to render new commits

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -70,6 +70,7 @@ const GitHub = () => {
                     >
                     </input>
                 </form>
+                <button onClick={() => setCallAPI(true)}>Refresh</button>
             </div>
                 {commits.length > 1 && displayName ? <h4>10 Most Recent Commits for {displayName}</h4> : null }
             <ul className="commit-list">
