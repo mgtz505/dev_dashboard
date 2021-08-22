2bea57477cea2f29eb570158c8a0448648a4c01a
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-22 14:12:11 -0400

    Reorganized / rewrote .spec files, currently utilizing jest to test components due to babel issues. Currently passing all test cases

--- src/testing/count.spec.js
+++ src/testing/count.spec.js
@@ -1,25 +1,16 @@
-// import React from "react";
-// import TestUtils from "react-addons-test-utils";
-// import expect from "expect";
-// // import Count from "../components/Count";
+import React from "react";
+import renderer from 'react-test-renderer';
+import expect from "expect";
+import Count from "../components/Count";
 
-// const Count = ({ count }) => {
-//     return (
-//         <div className="count-container">
-//             {count ? 
-//             (count === 1 ? <h4>{count} task remaining</h4> : <h4>{count} tasks remaining</h4> )
-//             : <h4>No Tasks Remaining</h4>}
-//         </div>
-//     )
-// }
+describe("The Count Component", () => {
 
-// describe("The Count Component", () => {
+    it("Component should render an integer count", () => {
 
-//     it("Component should render an integer count", () => {
+        const component = renderer.create(
 
-//         const renderer = TestUtils.createRenderer();
-//         renderer.render(<Count count={5}/>);
-//         const output = renderer.getRenderOutput();
-//         console.log(output);
-//     })
-// })
+            <Count count={5}/>
+        );
+        console.log(component);
+    })
+})
