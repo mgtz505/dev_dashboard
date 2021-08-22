321698ee1f6d9d7995221fc97d3fcd84ebfe7ba6
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-22 18:50:18 -0400

    Making Progress on formatting weather output

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -33,7 +33,7 @@ console.log(city)
     }
 
     return (
-        <div className="widgit">
+        <div className="widgit-long">
             {city.length > 1 ?<h2> Current Weather for {city}</h2> : <h2>Your Local Weather</h2>}
             <div className="city-form">
                 <form onSubmit={handleSubmit}>
@@ -48,16 +48,21 @@ console.log(city)
            {weatherData ? (
                <>
                <div className="weather-container">
-                   <h3>Currently</h3>
+                   <h3 className="container-header">Currently</h3>
+                   <h3 >{weatherData.current_condition[0].weatherDesc[0].value}</h3>
+                   <div className="grouping">
                    <h4>Actual Temp: {weatherData.current_condition[0].temp_F}°F | {weatherData.current_condition[0].temp_C}°C</h4>
-                   <h4>Humidity: {weatherData.current_condition[0].humidity}%</h4>
                    <h4>Feels like: {weatherData.current_condition[0].FeelsLikeF}°F | {weatherData.current_condition[0].FeelsLikeC}°C </h4>
+                   <h4>Humidity: {weatherData.current_condition[0].humidity}%</h4>
+                   <h4>Wind Speed: {weatherData.current_condition[0].windspeedMiles} mph</h4>
+                   </div>
                </div>
                <div className="weather-container">
-                    <h3>Today</h3>
-                    <h4>High: {weatherData.weather[0].maxtempF}°F | {weatherData.weather[0].maxtempC}°C </h4>
-                    <h4>Average: {weatherData.weather[0].avgtempF}°F | {weatherData.weather[0].avgtempC}°C </h4>
-                    <h4>Low: {weatherData.weather[0].mintempF}°F | {weatherData.weather[0].mintempC}°C </h4>
+                    <h3 className="container-header">Today</h3>
+                    <div className="grouping">
+                        <h4>High: {weatherData.weather[0].maxtempF}°F | {weatherData.weather[0].maxtempC}°C </h4>
+                        <h4>Low: {weatherData.weather[0].mintempF}°F | {weatherData.weather[0].mintempC}°C </h4>
+                    </div>
                 </div>
                 <button 
                 className="control-button" 
@@ -66,21 +71,23 @@ console.log(city)
                 <div>
                     {weatherData.weather.map((days, index) => {
                         return (
-                            <div className="forecast-container" key ={index}>
+                            <div  key ={index}>
                             <h4>Summary for {days.date.slice(5)}</h4>
                             <h4>High: {days.maxtempF}°F | {days.maxtempC}°C</h4>
                             <h4>Avg: {days.avgtempF}°F | {days.avgtempC}°C</h4>
                             <h4>Low: {days.mintempF}°F | {days.mintempC}°C</h4>
-                            <h4>Hourly Forecast:</h4>
-                                <div>
+                                <div className="forecast-container">
                                     {days.hourly.map((hour) => {
                                         return (
                                                 <div className="hour-container">
-                                                    <h3>{formatTime(hour.time)}</h3>
-                                                    <h4>{hour.weatherDesc[0].value} {hour.tempF}°F | {hour.tempC}°C</h4>
-                                                    <h5>Feels like: {hour.FeelsLikeF}°F | {hour.FeelsLikeC}°C</h5>
-                                                    <h5>Gusts: {hour.WindGustMiles} mph | WindChill: {hour.WindChillF}°F | {hour.WindChillC}°C</h5>
-                                                    <h5>Chance of Rain: {hour.chanceofrain}% | Precipitation: {hour.precipInches} in. </h5>
+                                                    <div className="grouping">
+                                                        <h3 className="container-header">{formatTime(hour.time)}</h3>
+                                                            <h4>{hour.weatherDesc[0].value} </h4>
+                                                            <h5>{hour.tempF}°F | {hour.tempC}°C</h5>
+                                                            <h5>Feels like: {hour.FeelsLikeF}°F | {hour.FeelsLikeC}°C</h5>
+                                                            <h5>Gusts: {hour.WindGustMiles} mph </h5>
+                                                            <h5>Chance of Rain: {hour.chanceofrain}% | Precipitation: {hour.precipInches} in. </h5>
+                                                    </div>
                                                 </div>
                                         )
                                     })}
