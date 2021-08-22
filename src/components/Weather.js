8dbba55bc82ca2b13f0397bf2bbc959bc24cc618
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-22 15:59:30 -0400

    API call succeeded. About to begin displaying information and formatting results

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 
 const Weather = () => {
+
+const [weatherData, setWeatherData] = useState();
+
+const URL = "http://wttr.in/London?format=j1"
+
+const getWeather = () => {
+    axios.get(URL).then((response) => {
+        console.log(response.data)
+        console.log(response.data.current_condition[0].FeelsLikeF)
+        setWeatherData(response.data)
+    })
+}
+console.log(weatherData);
+
     return (
         <div className="widgit">
             <h2>See Weather</h2>
+           {weatherData ? (
+               <div className="weather-data">
+                   <h3>{weatherData.current_condition[0].FeelsLikeF}</h3>
+               </div>
+           )
+            : null}
+            
+            <button className="control-button" onClick={() => getWeather()}>Get Weather</button>
         </div>
     );
 };
