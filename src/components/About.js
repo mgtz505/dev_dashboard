3c82104edc20c30fd8b57597c0017a89f8dbf699
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-23 14:39:18 -0400

    About to begin syling process, palette selected

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,8 @@ const About = () => {
         <div className="aboutPage">
 
             <h2>About Dev Dashboard</h2>
-            
+            <button className="test">Hello there</button>
+     
         </div>
     );
 };
