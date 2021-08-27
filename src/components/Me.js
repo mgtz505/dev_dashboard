a1bcb34fe0ae196398bab88ddbb6e5bbf6954bd5
mgtz505 <gutierrez.marcusc@gmail.com>
2021-08-27 19:27:45 -0400

    General formatting, fixed the -index issue w modal button and made sure that both about.js and me.js are formatted properly

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "../styles/me.css";
+import "../styles/about.css";
+import nyc from "../styles/images/nyc.jpg"
+
 
 const Me = () => {
     return (
-        <div className="mePage">
-            <h2>Marcus</h2>
+        <div className="aboutPage">
+            <div className="aboutPage">
+
+<br></br>
+<div className="about-container">
+<div className="about-text-container">
+<h2 className="about-header">Thanks for checking out Dev Dashboard!</h2>
+    <h3>I'm Marcus Gutierrez | A little about me:</h3>
+    <ul>
+        <li>✔️ I'm an aspiring software engineer looking to grow my programming skills and learn</li>
+        <li>✔️ I made a career shift from finance after nearly four years as a proprietary trader</li>
+        <li>✔️ I graduated from Harvard in 2017, concentrating in Economics with a secondary in Global Health & Health Policy </li>
+        <li>✔️ I'm looking to kickstart my SWE career in the NYC metro area, or perhaps working for a remote team</li>
+        <li>✔️ I love to learn in public! Follow the link below to see what I've been working on</li>
+        <a href="https://github.com/mgtz505" target="_blank" rel="noreferrer">My GitHub Profile</a>
+       
+    </ul>
+</div>
+<img className="about-image" src={nyc} />  
+</div>
+</div>
         </div>
     );
 };
